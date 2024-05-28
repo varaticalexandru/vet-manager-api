a5008de41f6569d156f89750080747f8962958f0
alex <varaticalexandru1@gmail.com>
2024-05-28 20:52:51 +0300

    project init

---
+++ src/main/java/org/alexv/vet_manager_api/VetManagerApiApplication.java
@@ -0,0 +1,13 @@
+package org.alexv.vet_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alexv/vet_manager_api/VetManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexv.vet_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
