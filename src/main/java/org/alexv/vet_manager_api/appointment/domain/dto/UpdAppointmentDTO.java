78bb2f96d098ffcba3758043013bb920ba9e3320
alex <varaticalexandru1@gmail.com>
2024-06-04 17:31:59 +0300

    add update and delete endpoints, services, DTOs

--- src/main/java/org/alexv/vet_manager_api/appointment/domain/dto/UpdAppointmentDTO.java
+++ src/main/java/org/alexv/vet_manager_api/appointment/domain/dto/UpdAppointmentDTO.java
@@ -3,23 +3,26 @@ package org.alexv.vet_manager_api.appointment.domain.dto;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.alexv.vet_manager_api.appointment.domain.entity.AppointmentStatus;
+import org.alexv.vet_manager_api.service.domain.dto.NewServiceDTO;
 
 import java.time.Instant;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class AppointmentUpdateDTO {
+public class UpdAppointmentDTO {
 
     Long id;
-    Long petId;
-    Long doctorId;
-    String petName;
-    String doctorFirstName;
-    String doctorLastName;
     Instant date;
+    Boolean newPet;
+    Object pet;
+    Boolean newDoctor;
+    Object doctor;
+    List<Long> services;
+    List<NewServiceDTO> newServices;
     AppointmentStatus status;
     String diagnostic;
 }
