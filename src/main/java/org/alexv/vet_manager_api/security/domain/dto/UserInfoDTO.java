f4b4d9624fe94525664f9568cd5ffa069d8bbfa5
alex <varaticalexandru1@gmail.com>
2024-06-06 12:55:23 +0300

    add UserInfo dto for creation of new users

---
+++ src/main/java/org/alexv/vet_manager_api/security/domain/dto/UserInfoDTO.java
@@ -0,0 +1,16 @@
+package org.alexv.vet_manager_api.security.domain.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserInfoDTO {
+
+    String email;
+    String username;
+    String password;
+}
